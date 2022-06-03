dac5ec015b6dab05be3f3d1c9be00da3e409786c
mr-francois <mr-francois@gmx.de>
2022-06-03 18:56:09 +0200

    renaming

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -1,13 +1,10 @@
 context("training")
 
 test_that("Sucessful training from a dummy model", {
-   
-   skip_if_no_keras()
-   skip_on_travis()
-   
+
    maxlen <- 30
    batch_size <- 10
-   
+
    model <- create_model_lstm_cnn(
       maxlen = maxlen,
       layer_dense = 4,
@@ -15,23 +12,23 @@ test_that("Sucessful training from a dummy model", {
       solver = "adam",
       vocabulary_size = 4,
       compile = TRUE)
-   
-   trainedNetwork <- train(reduce_lr_on_plateau = FALSE,
-                           train_type = "lm",
-                           path = "/home/rmreches/deepG/tests/testthat/fasta",
-                           path_val = "/home/rmreches/deepG/tests/testthat/fasta",
-                           model = model,
-                           train_val_ratio = 0.2,
-                           steps_per_epoch = 3,
-                           batch_size = batch_size,
-                           epochs = 1)
-   
+
+   trainedNetwork <- train_model(reduce_lr_on_plateau = FALSE,
+                                 train_type = "lm",
+                                 path = "fasta",
+                                 path_val = "fasta",
+                                 model = model,
+                                 train_val_ratio = 0.2,
+                                 steps_per_epoch = 3,
+                                 batch_size = batch_size,
+                                 epochs = 1)
+
    expect_type(trainedNetwork, "list")
    expect_type(trainedNetwork[["metrics"]][["loss"]], "double")
    expect_gte(trainedNetwork[["metrics"]][["loss"]], 0)
    expect_type(trainedNetwork[["metrics"]][["val_loss"]], "double")
    expect_gte(trainedNetwork[["metrics"]][["val_loss"]], 0)
-   
+
    model <- create_model_lstm_cnn(
       maxlen = maxlen,
       kernel_size = c(4,4),
@@ -42,18 +39,18 @@ test_that("Sucessful training from a dummy model", {
       solver = "adam",
       vocabulary_size = 4,
       compile = TRUE)
-   
-   trainedNetwork <- train(reduce_lr_on_plateau = FALSE,
-                           train_type = "label_folder",
-                           path = rep("/home/rmreches/deepG/tests/testthat/fasta", 2),
-                           path_val = rep("/home/rmreches/deepG/tests/testthat/fasta", 2),
-                           model = model,
-                           vocabulary_label = c("A", "B"),
-                           train_val_ratio = 0.2,
-                           steps_per_epoch = 3,
-                           batch_size = batch_size,
-                           epochs = 1)
-   
+
+   trainedNetwork <- train_model(reduce_lr_on_plateau = FALSE,
+                                 train_type = "label_folder",
+                                 path = rep("fasta", 2),
+                                 path_val = rep("fasta", 2),
+                                 model = model,
+                                 vocabulary_label = c("A", "B"),
+                                 train_val_ratio = 0.2,
+                                 steps_per_epoch = 3,
+                                 batch_size = batch_size,
+                                 epochs = 1)
+
    expect_type(trainedNetwork, "list")
    expect_type(trainedNetwork[["metrics"]][["loss"]], "double")
    expect_gte(trainedNetwork[["metrics"]][["loss"]], 0)
