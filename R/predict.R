bb553b7850ea81cef62706ed3c1a1a81e34ab700
mr-francois <mr-francois@gmx.de>
2022-10-18 18:51:12 +0200

    add arguments for encoding

--- R/predict.R
+++ R/predict.R
@@ -658,9 +658,9 @@ predict_model_one_pred_per_entry <- function(model = NULL, layer_name = NULL, pa
       if (i == 1) start_ind <- seq(1, nchar(char_seq), maxlen)
       one_hot_batch <- seq_encoding_label(sequence = NULL, maxlen = maxlen, vocabulary = vocabulary,
                                           start_ind = start_ind, ambiguous_nuc = ambiguous_nuc, nuc_dist = NULL,
-                                          use_quality = FALSE, quality_vector = NULL, use_coverage = FALSE, max_cov = NULL,
+                                          quality_vector = NULL, use_coverage = FALSE, max_cov = NULL,
                                           cov_vector = NULL, n_gram = NULL, n_gram_stride = 1, char_sequence = char_seq,
-                                          tokenizer = tokenizer, adjust_start_ind = TRUE) 
+                                          tokenizer = tokenizer, adjust_start_ind = TRUE, ...) 
       if (reverse_complement_encoding) one_hot_batch <- list(one_hot_batch, reverse_complement_tensor(one_hot_batch))
       activations <- keras::predict_on_batch(model, one_hot_batch)
       writer[row : (row + batch_size - 1), ] <- activations
