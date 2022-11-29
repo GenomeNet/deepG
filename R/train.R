1e03ec5b54737c691794e0461886976e44e416c9
xiaoyint <xjt@web.de>
2022-11-29 17:13:31 +0100

    add package

--- R/train.R
+++ R/train.R
@@ -1105,7 +1105,7 @@ train_model_cpc <-
                 tensorflow::tf$summary$scalar(
                   'training files seen',
                   nrow(
-                    read_csv(
+                    readr::read_csv(
                       path_file_log,
                       col_names = FALSE,
                       col_types = cols()
